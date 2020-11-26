a7f0f798349ded657f55e2b55dbd271aa9fd90d9
Edward Viaene <ward@in4it.io>
2020-11-26 18:03:16 +0100

    default action for lb

--- modules/alb/lb.tf
+++ modules/alb/lb.tf
@@ -19,16 +19,31 @@ data "aws_acm_certificate" "certificate" {
 
 # lb listener (https)
 resource "aws_lb_listener" "lb-https" {
-  count = var.ssl ? 1 : 0
+  count             = var.ssl ? 1 : 0
   load_balancer_arn = aws_lb.lb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "elbsecuritypolicy-2016-08"
   certificate_arn   = data.aws_acm_certificate.certificate.arn
 
-  default_action {
-    target_group_arn = var.default_target_arn
-    type             = "forward"
+  dynamic default_action {
+    for_each = var.default_target_arn == "" ? [] : [1]
+    content {
+      target_group_arn = var.default_target_arn
+      type             = "forward"
+    }
+  }
+
+  dynamic default_action {
+    for_each = var.default_target_arn == "" ? [1] : []
+    content {
+      type = "fixed-response"
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "No service configured at this address"
+        status_code  = 404
+      }
+    }
   }
 }
 
