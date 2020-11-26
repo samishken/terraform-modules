ea8d4e456c077be21764a1e055421f30ebd0f0b7
Jorn Jambers <jorn@in4it.io>
2020-11-26 15:16:52 +0100

    make ssl optional

--- modules/alb/lb.tf
+++ modules/alb/lb.tf
@@ -19,7 +19,7 @@ data "aws_acm_certificate" "certificate" {
 
 # lb listener (https)
 resource "aws_lb_listener" "lb-https" {
-  count = var.ssl ? 1 : null
+  count = var.ssl ? 1 : 0
   load_balancer_arn = aws_lb.lb.arn
   port              = "443"
   protocol          = "HTTPS"
