5a9cf7133b1a825cf1f869222d56de5cff8c8602
Edward Viaene <ward@in4it.io>
2020-11-26 09:54:09 +0100

    ssm

---
+++ modules/ssm-parameters/kms.tf
@@ -0,0 +1,13 @@
+resource "aws_kms_key" "ssm-parameters" {
+  count                   = var.at_rest_encryption ? 1 : 0
+  description             = "SSM Paramstore key ${var.prefix}"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+}
+resource "aws_kms_alias" "ssm-parameters" {
+  count         = var.at_rest_encryption ? 1 : 0
+  name          = "alias/ssm-paramstore-${var.prefix}"
+  target_key_id = aws_kms_key.rds[0].key_id
+}
+
+
