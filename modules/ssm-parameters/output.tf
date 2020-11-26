8a911460ca9667d3591fdb30c34ee36cee67eae9
Edward Viaene <ward@in4it.io>
2020-11-26 16:33:06 +0100

    output fixes

--- modules/ssm-parameters/output.tf
+++ modules/ssm-parameters/output.tf
@@ -1,6 +1,6 @@
 output "kms-key-id" {
-  value = aws_kms_key.ssm-parameters.key_id
+  value = length(aws_kms_key.ssm-parameters) > 0 ? aws_kms_key.ssm-parameters[0].key_id : null
 }
 output "kms-key-arn" {
-  value = aws_kms_key.ssm-parameters.arn
+  value = length(aws_kms_key.ssm-parameters) > 0 ? aws_kms_key.ssm-parameters[0].arn : null
 }
